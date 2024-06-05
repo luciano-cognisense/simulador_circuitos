20d6ae1b1e07ea7f2e4d6dcb0095928de112d712
luciano-cognisense <luciano.fpolis@gmail.com>
2024-06-05 16:22:48 -0300

    Add files via upload

--- simulador.js
+++ simulador.js
@@ -19,15 +19,16 @@ class Component {
 }
 
 class Node {
-	constructor(name, component, terminal, childrenList, childrenNodes, previousNode){
+	constructor(name, input_component, output_component, input_terminal,output_terminal,childrenList, childrenNodes, previousNode){
 		this.name = name;
-		this.component = component;
-		this.terminal = terminal;
+		this.input_component = input_component;
+		this.output_component = output_component;
+		this.input_terminal = input_terminal;
+		this.output_terminal = output_terminal;
 		this.childrenList = childrenList;
 		this.childrenNodes = childrenNodes;
 		this.previousNode = previousNode;
 	};
-	
 };
 
 // Cables Vector
@@ -68,6 +69,7 @@ const componentTerminals = {
 	circuit_breaker: circuit_breaker_pins
 };
 
+// Atualiza dropdown list na página web em função do componente escolhido
 function updateTerminals(componentSelectId, terminalSelectId) {
     const componentSelect = document.getElementById(componentSelectId);
     const terminalSelect = document.getElementById(terminalSelectId);
@@ -88,6 +90,7 @@ function updateTerminals(componentSelectId, terminalSelectId) {
     }
 }
 
+// Cria o cabo independente da interface (Modo teste)
 function createCable(component1, terminal1, component2, terminal2, voltage){
 		const connectionsDiv = document.getElementById('connections');
 		const connectionElement = document.createElement('p');
@@ -101,9 +104,11 @@ function createCable(component1, terminal1, component2, terminal2, voltage){
 		connectionsDiv.appendChild(connectionElement);
 }
 
+// Inicializa as conexões em modo teste
 function initializeConnections(){
 		//Montagem padrão de nível 3 para testes
-		createCable("power_supply", "24V", "button_1", "11", null);
+		//createCable("power_supply", "24V", "button_1", "11", null);
+		createCable("button_1", "11", "power_supply", "24V", null);
 		createCable("power_supply", "24V", "button_1", "13", null);
 		createCable("button_1", "12", "led_2", "X1", null);
 		createCable("button_1", "14", "led_1", "X1", null);
@@ -112,6 +117,7 @@ function initializeConnections(){
 }
 initializeConnections();
 
+// Cria conexões ao apertar o botão "Criar conexão" na interface web
 function createConnection() {
     const component1 = document.getElementById('component-select-1').value;
     const terminal1 = document.getElementById('terminal-select-1').value;
@@ -134,36 +140,7 @@ function createConnection() {
     }
 }
 
-const root = new Node("root", null, null, [], [], null); 
-
-function createCircuitRoot(){
-	//name, component, terminal, childrenList, childrenNodes, previousNode
-	for(var i=0; i<cables.length;i++){
-		if(cables[i].output_component == "power_supply" && checkOutputTerminal(cables[i].output_terminal) || 
-			cables[i].input_component == "power_supply" && checkOutputTerminal(cables[i].input_terminal)){
-			
-			if(cables[i].input_component == "power_supply"){
-				revertCable(cables[i]);
-			}
-
-			//console.log(cables[i]);
-			let newNode = new Node("power_supply_" + String(cables[i].output_terminal), getComponentbyName("power_supply"), cables[i].output_terminal, [], [], root);
-			if(cables[i].output_terminal == "24V"){
-				newNode.component.voltage = "24V";
-			}else{
-				newNode.component.voltage = "380V"
-			}
-			root.childrenList.push(newNode.name);
-			root.childrenNodes.push(newNode);
-		}
-	}
-	
-	//console.log(root);
-	for(var i=0; i<root.childrenNodes.length;i++){
-		createCircuitTree(root.childrenNodes[i]);
-	}
-}
-
+// Verifica se um terminal é output ou input do component
 function checkOutputTerminal(terminal){
 	//console.log(terminal);
 	for(var i=0; i<power_supply_output_pins.length;i++){
@@ -189,6 +166,8 @@ function checkOutputTerminal(terminal){
 	return false;
 }
 
+// Reverte os cabos. 
+// Utilizado para alinhar a origem e destino do cabo com o caminho da corrente
 function revertCable(cable){
 	let initial_input_component = cable.input_component;
 	let initial_input_terminal = cable.input_terminal;
@@ -201,6 +180,8 @@ function revertCable(cable){
 	cable.output_terminal = initial_input_terminal;
 }
 
+
+// Retorna o objeto componente ao receber o nome (string) dele
 function getComponentbyName(name){
 	switch(name){
 		case "power_supply": return power_supply;
@@ -214,44 +195,147 @@ function getComponentbyName(name){
 	}
 }
 
-function checkComponentOutputTerminal(component_name, component_input_terminal){
+const root = new Node("root", null, null, null, null, [], [], null); 
 
-}
-
-function createCircuitTree(currentNode){
-	//console.log(currentNode);
-	// Node constructor(name, component, terminal, childrenList, childrenNodes, previousNode)
-	// Component constructor(name,input_pins,output_pins,voltage,state)
-
-	for(var i=0; i<cables_check.length;i++){
-		if(cables_check[i].output_component == currentNode.component.name && checkOutputTerminal(cables_check[i].output_terminal) || 
-		cables_check[i].input_component == currentNode.component.name && checkOutputTerminal(cables_check[i].input_terminal)){
+// Cria a raiz do circuito
+function createCircuitRoot(){
+	//name, input_component, output_component, input_terminal,output_terminal, childrenList, childrenNodes, previousNode
+	for(var i=0; i<cables.length;i++){
+		//Acha os cabos conectados ao módulo de alimentação
+		if(cables[i].output_component == "power_supply" && checkOutputTerminal(cables[i].output_terminal) || 
+			cables[i].input_component == "power_supply" && checkOutputTerminal(cables[i].input_terminal)){
 			
-			if(cables_check[i].input_component == currentNode.component.name){
-				revertCable(cables_check[i]);
+			//Reverte os cabos para que o módulo de alimentação seja output
+			if(cables[i].input_component == "power_supply"){
+				revertCable(cables[i]);
 			}
 
-			// Cria novo node filho a partir do cabo
-			let newNode = new Node(String(cables_check[i].input_component) + "_" + String(cables_check[i].input_terminal), getComponentbyName(cables_check[i].input_component), 
-									String(cables_check[i].input_terminal), [], [], currentNode);
-			
-			currentNode.childrenList.push(newNode.name);
-			currentNode.childrenNodes.push(newNode);
+			//Cria um novo nó para o circuito
+			//let newNode = new Node("power_supply_" + String(cables[i].output_terminal), cables[i].input_component, "power_supply", cables[i].input_terminal, cables[i].output_terminal, [], [], root);
+			let newNode = new Node("power_supply_" + String(cables[i].output_terminal), null, "power_supply", null, cables[i].output_terminal, [], [], root);
+			//console.log("Cria Node");
+			//console.log(newNode);
 
-			// Exclui cabo da check_list
-			let new_array = cables_check.filter(function(current_cable){return current_cable !== cables_check[i];});
-			cables_check = new_array;
+			if(cables[i].output_terminal == "24V"){
+				newNode.output_component.voltage = "24V";
+			}else{
+				newNode.output_component.voltage = "380V"
+			}
+
+			//Estabelece o novo nó como filho da raiz
+			root.childrenList.push(newNode.name);
+			root.childrenNodes.push(newNode);
 		}
 	}
 	
-	console.log(root);
+	//console.log(root);
+	for(var i=0; i<root.childrenNodes.length;i++){
+		createCircuitTree(root.childrenNodes[i]);
+	}
+}
+
+// A partir da entrada do componente retorna qual a saída esperada
+function getComponentOutputTerminal(component_name, component_input_terminal){
+	switch(component_name){
+		case "power_supply": return component_input_terminal;
+		case "led_1": return "X2";
+		case "led_2": return "X2";
+		case "led_3": return "X2";
+		case "button_1": switch(component_input_terminal){
+								case "11": return "12";
+								case "13": return "14";
+								case "21": return "22";
+								case "23": return "24";
+						}
+		case "button_2": return button_2;
+		case "circuit_breaker": return circuit_breaker;
+		default: return null;
+	}
+}
+
+function createCircuitTree(currentNode){
+	console.log(currentNode);
+	for(var i=0; i<cables_check.length;i++){
+		/* if(cables_check[i].output_component == currentNode.output_component && checkOutputTerminal(cables_check[i].output_terminal) || 
+			cables_check[i].input_component == currentNode.output_component && checkOutputTerminal(cables_check[i].input_terminal)){  */
+			if(cables_check[i].output_component == currentNode.output_component && cables_check[i].output_terminal == currentNode.output_terminal || 
+			cables_check[i].input_component == currentNode.output_component && cables_check[i].input_terminal == currentNode.output_terminal){
+				//console.log(cables_check[i]);
+				//console.log(checkOutputTerminal(cables_check[i].output_terminal));
+				//console.log(checkOutputTerminal(cables_check[i].input_terminal));
+				// Reverte os cabos para que entradas e saídas estejam ordenadas ao caminho da corrente
+				if(cables_check[i].input_component == currentNode.output_component){
+				 	console.log(cables_check[i]);
+				 	revertCable(cables_check[i]);
+				 	console.log("reverteu");
+				 	console.log(cables_check[i]);
+				}
+
+
+				
+				//Atualiza currentNode com o component e terminal de entrada
+				currentNode.input_component = cables_check[i].input_component;
+				currentNode.input_terminal = cables_check[i].input_terminal;
+				//console.log("Atualiza Node");
+				//console.log(currentNode);
+
+				//Pega o terminal de saída do novo componente
+				//console.log(cables_check[i]);
+				let component_output_terminal = getComponentOutputTerminal(cables_check[i].input_component, cables_check[i].input_terminal)
+				
+				// Cria novo nó filho a partir do cabo
+				//name, input_component, output_component, input_terminal,output_terminal, childrenList, childrenNodes, previousNode
+				let newNode = new Node(cables_check[i].input_component + "_" + cables_check[i].input_terminal, null,cables_check[i].input_component, 
+				null,component_output_terminal, [], [],currentNode);
+				//console.log("Cria Node");
+				//console.log(newNode);
+				
+
+				// Atualiza nós filhos 
+				// onsole.log(currentNode);
+				//console.log(currentNode.childrenList);
+				currentNode.childrenList.push(newNode.name);
+				currentNode.childrenNodes.push(newNode);
+
+				// Exclui cabo da check_list
+				let new_array = cables_check.filter(function(current_cable){return current_cable !== cables_check[i];});
+				cables_check = new_array;
+				
+				console.log(newNode);
+				if(checkOutputTerminal(newNode.output_terminal)){
+					createCircuitTree(newNode);
+				}
+				else{
+					//console.log("Componente não possui output");
+				}
+			}
+		//}	
+	}
+
+	//console.log(root);
+	//getMatrix(root);
 	//printCircuit();
 }
 
+
 function printCircuit(){
-	let current_Node = root;
+	createCircuitRoot();
 	
-	while(current_Node.childrenNodes != null){
-		
+	let nivelAtual = [root];
+	//console.log(root);
+	while (nivelAtual.length > 0) {
+		//console.log(nivelAtual);
+		const proximoNivel = [];
+		let saida = "";
+
+		for (let no of nivelAtual) {
+			//console.log(no);
+			saida += no.name + "\t";
+			proximoNivel.push(...no.childrenNodes);
+		}
+
+		console.log(saida.trim());
+		nivelAtual = proximoNivel;
 	}
+
 }
